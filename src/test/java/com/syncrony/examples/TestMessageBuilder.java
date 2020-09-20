01553e4f42238bc49f3f9359b330b58e464ba9d9
skolapar <srinivas.kolaparthi@capgemini.com>
2020-09-20 08:09:28 +0530

    aaa

---
+++ src/test/java/com/syncrony/examples/TestMessageBuilder.java
@@ -0,0 +1,35 @@
+package com.syncrony.examples;
+
+import org.junit.jupiter.api.Test;
+
+import com.syncrony.examples.MessageBuilder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestMessageBuilder {
+
+    @Test
+    public void testNameMkyong() {
+
+        MessageBuilder obj = new MessageBuilder();
+        assertEquals("Hello mkyong", obj.getMessage("mkyong"));
+
+    }
+
+    /*@Test
+    public void testNameEmpty() {
+
+        MessageBuilder obj = new MessageBuilder();
+        assertEquals("Please provide a name!", obj.getMessage(" "));
+
+    }
+
+    @Test
+    public void testNameNull() {
+
+        MessageBuilder obj = new MessageBuilder();
+        assertEquals("Please provide a name!", obj.getMessage(null));
+
+    }*/
+
+}
